7c30c7715f2456439c96d498c67cd0af701fa55c
maarjatapsi <maarja.tapsi@gmail.com>
2021-04-30 11:50:33 +0300

    Validation Error

--- src/components/__tests__/NewRestaurantForm.spec.js
+++ src/components/__tests__/NewRestaurantForm.spec.js
@@ -5,6 +5,7 @@ import {NewRestaurantForm} from '../NewRestaurantForm';
 
 describe('NewRestaurantForm', () => {
   const restaurantName = 'Sushi Place';
+  const requiredError = 'Name is required';
 
   let createRestaurant;
   let context;
@@ -12,6 +13,12 @@ describe('NewRestaurantForm', () => {
   beforeEach(() => {
     createRestaurant = jest.fn().mockName('createRestaurant');
     context = render(<NewRestaurantForm createRestaurant={createRestaurant} />);
+  });
+  describe('initially', () => {
+    it('does not display a validation error', () => {
+      const {queryByText} = context;
+      expect(queryByText(requiredError)).toBeNull();
+    });
   });
   describe('when filled in', () => {
     beforeEach(async () => {
@@ -27,13 +34,59 @@ describe('NewRestaurantForm', () => {
 
       return act(flushPromises);
     });
-
+    it('does not display a validation error', () => {
+      const {queryByText} = context;
+      expect(queryByText(requiredError)).toBeNull();
+    });
     it('calls createRestaurant with the name', () => {
       expect(createRestaurant).toHaveBeenCalledWith(restaurantName);
     });
     it('clears the name', () => {
       const {getByPlaceholderText} = context;
       expect(getByPlaceholderText('Add Restaurant').value).toEqual('');
+    });
+  });
+  describe('when empty', () => {
+    beforeEach(async () => {
+      createRestaurant.mockResolvedValue();
+
+      const {getByPlaceholderText, getByTestId} = context;
+      await userEvent.type(getByPlaceholderText('Add Restaurant'), '');
+      userEvent.click(getByTestId('new-restaurant-submit-button'));
+
+      return act(flushPromises);
+    });
+
+    it('displays a validation error', () => {
+      const {queryByText} = context;
+      expect(queryByText(requiredError)).not.toBeNull();
+    });
+    it('does not call createRestaurant', () => {
+      expect(createRestaurant).not.toHaveBeenCalled();
+    });
+  });
+  describe('when correcting a validation error', () => {
+    beforeEach(async () => {
+      createRestaurant.mockResolvedValue();
+
+      const {getByPlaceholderText, getByTestId} = context;
+
+      await userEvent.type(getByPlaceholderText('Add Restaurant'), '');
+      userEvent.click(getByTestId('new-restaurant-submit-button'));
+      await act(flushPromises);
+
+      await userEvent.type(
+        getByPlaceholderText('Add Restaurant'),
+        restaurantName,
+      );
+      userEvent.click(getByTestId('new-restaurant-submit-button'));
+
+      return act(flushPromises);
+    });
+
+    it('clears the validation error', () => {
+      const {queryByText} = context;
+      expect(queryByText(requiredError)).toBeNull();
     });
   });
 });
