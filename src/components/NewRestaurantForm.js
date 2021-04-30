5d03dde81c4350c2a07e0a6974e26420e4de89ee
maarjatapsi <maarja.tapsi@gmail.com>
2021-04-30 10:29:47 +0300

    Unit Testing the Component

--- src/components/NewRestaurantForm.js
+++ src/components/NewRestaurantForm.js
@@ -1,11 +1,29 @@
+import {useState} from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
-export const NewRestaurantForm = () => {
+export const NewRestaurantForm = ({createRestaurant}) => {
+  const [name, setName] = useState('');
+  const handleSubmit = e => {
+    e.preventDefault();
+    createRestaurant(name);
+  };
+
   return (
-    <form>
-      <TextField placeholder="Add Restaurant" fullWidth variant="filled" />
-      <Button variant="contained" color="primary">
+    <form onSubmit={handleSubmit}>
+      <TextField
+        value={name}
+        onChange={e => setName(e.target.value)}
+        placeholder="Add Restaurant"
+        fullWidth
+        variant="filled"
+      />
+      <Button
+        type="submit"
+        variant="contained"
+        color="primary"
+        data-testid="new-restaurant-submit-button"
+      >
         Add
       </Button>
     </form>
